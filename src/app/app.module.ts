80d83c756c913848a6db812c1f22b3c5787b53d8
Hiki <91337467+Hiki-Rocio@users.noreply.github.com>
2022-08-08 19:53:01 -0300

    creacion del perfil

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componente/header/header.component';
+import { LogoArgProgComponent } from './componente/logo-arg-prog/logo-arg-prog.component';
+import { RedesComponent } from './componente/redes/redes.component';
+import { BannerComponent } from './componente/banner/banner.component';
+import { AcercaDeComponent } from './componente/acerca-de/acerca-de.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    RedesComponent,
+    BannerComponent,
+    AcercaDeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
