b1b9223f2674bc09a7290c135a0ae4115baa9d8e
Hiki <91337467+Hiki-Rocio@users.noreply.github.com>
2022-08-11 18:37:52 -0300

    finfront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componente/header/header.component';
 import { LogoArgProgComponent } from './componente/logo-arg-prog/logo-arg-prog.component';
-import { RedesComponent } from './componente/redes/redes.component';
 import { BannerComponent } from './componente/banner/banner.component';
 import { AcercaDeComponent } from './componente/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './componente/experiencia/experiencia.component';
@@ -13,19 +12,20 @@ import { EducacionComponent } from './componente/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftComponent } from './componente/hard-soft/hard-soft.component';
 import { ProyectoComponent } from './componente/proyecto/proyecto.component';
+import { FooterComponent } from './componente/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArgProgComponent,
-    RedesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
     HardSoftComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
